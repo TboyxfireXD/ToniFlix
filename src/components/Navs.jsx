93bbd6f9b1b0989708a7b2b1c53f22d137ae2ca1
TboyxfireXD <116435750+TboyxfireXD@users.noreply.github.com>
2024-09-07 11:58:35 +0200

    hopefully half way there

--- src/components/Navs.jsx
+++ src/components/Navs.jsx
@@ -9,60 +9,40 @@ function Navs() {
   return (
     <Navbar expand="lg" bg="transparent" className="custom-navbar">
       <Container className="justify-content-center">
-        <Navbar.Brand href="/" className="mx-auto">
+        <Navbar.Brand as={Link} to="/" className="mx-auto">
           ToniFlix
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
             <NavDropdown title="Movies" id="basic-nav-dropdown">
-              <NavDropdown.Item>
-                <Link to="/movies/upcoming"> Upcoming</Link>
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/movies/upcoming">Upcoming</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/movies/popular">
-                Popular
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/movies/popular">Popular</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/movies/top_rated">
-                Top Rated
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/movies/top_rated">Top Rated</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/movies/now_playing">
-                Now Playing
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/movies/now_playing">Now Playing</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/movies/trending">
-                Trending
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/movies/trending">Trending</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/movies/search">Search</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/movies/search">Search</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="TV Shows" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/tvshows/first_air_date">
-                First Air Date
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/tvshows/first_air_date">First Air Date</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/tvshows/on_the_air">
-                On The Air
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/tvshows/on_the_air">On The Air</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/tvshows/popular">
-                Popular
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/tvshows/popular">Popular</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/tvshows/top_rated">
-                top_rated
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/tvshows/top_rated">Top Rated</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/tvshows/trending">
-                Trending
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/tvshows/trending">Trending</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/tvshows/search">Search</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/tvshows/search">Search</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/signup">Sign Up</Nav.Link>
+            <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
